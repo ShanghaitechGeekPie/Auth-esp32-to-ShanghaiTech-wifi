8d5a24b3523e827a8ed75c5bfc36a3176068b6f6
Luxcoldury <302606271@qq,com>
2020-05-24 19:34:27 +0800

    timeout option (w/ rtos) for eduroam connection

--- src/eduroam.cpp
+++ src/eduroam.cpp
@@ -1,14 +1,15 @@
 #include <eduroam.h>
 
-void connect_to_eduroam(const char *eduroam_id, const char *eduroam_password){
+void connect_to_eduroam_task(void* void_edrp){
   const char* ssid = "eduroam";
+  eduroam_recipe *edrp = (eduroam_recipe *)void_edrp;
 
   // WPA2 enterprise magic starts here
   WiFi.disconnect(true);
   WiFi.mode(WIFI_STA);
-  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)eduroam_id, strlen(eduroam_id));
-  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)eduroam_id, strlen(eduroam_id));
-  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)eduroam_password, strlen(eduroam_password));
+  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)edrp->eduroam_id, strlen(edrp->eduroam_id));
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)edrp->eduroam_id, strlen(edrp->eduroam_id));
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)edrp->eduroam_password, strlen(edrp->eduroam_password));
   esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
   esp_wifi_sta_wpa2_ent_enable(&config);
 
@@ -17,4 +18,27 @@ void connect_to_eduroam(const char *eduroam_id, const char *eduroam_password){
   while (WiFi.status() != WL_CONNECTED) {
     delay(200);
   }
+
+  if(edrp->caller) xTaskNotifyGive(edrp->caller);
+}
+
+
+bool connect_to_eduroam(const char *eduroam_id, const char *eduroam_password){
+  eduroam_recipe edrp = {eduroam_id, eduroam_password, NULL};
+  connect_to_eduroam_task(&edrp);
+  return true;
+}
+
+
+bool connect_to_eduroam(const char *eduroam_id, const char *eduroam_password, int timeout){
+  TaskHandle_t edu_connect_task;
+  TickType_t timeout_ticks = timeout / portTICK_PERIOD_MS;
+  eduroam_recipe edrp = {eduroam_id, eduroam_password, xTaskGetCurrentTaskHandle()};
+
+  xTaskCreate(connect_to_eduroam_task, "eduroam_connecting_task", 2048, &edrp, 0, &edu_connect_task);
+  int success =  ulTaskNotifyTake(pdTRUE, timeout_ticks);
+  Serial.println("notify get:");
+  Serial.println(success);
+  vTaskDelete(edu_connect_task);
+  return (bool)success;
 }
--- src/eduroam.h
+++ src/eduroam.h
@@ -3,9 +3,51 @@
 
 #include <WiFi.h>
 #include "esp_wpa2.h"
-// #include "esp_log.h"
-// #include "connectivity.h"
+#include "connectivity_util.h"
 
-void connect_to_eduroam(const char *eduroam_id, const char *eduroam_password);
+struct eduroam_recipe{
+  const char *eduroam_id;
+  const char *eduroam_password;
+  TaskHandle_t caller;
+};
+
+/**
+  * @brief  Basic eduroam connection task
+  * 
+  * @param  void_edac: An eduroam_account sturct ptr should be passed through this param.
+  */
+void connect_to_eduroam_task(void* void_edac);
+
+/**
+  * @brief  Connect to eduroam. (Blocking!)
+  * 
+  * @attention 1. The core you run this function on will be BLOCKED until eduroam connection established.
+  * @attention 2. This function does NOT append "@shanghaitech.edu.cn" to eduroam_id for you, plz include it on your own.
+  * @attention 3. Your eduroam password MAY BE DIFFERENT w/ your ShanghaiTech password, egate password or GAuth password, be careful.
+  *
+  * @param  eduroam_id: Your eduroam id, which is also your edu email address. 
+  * @param  eduroam_password: Your eduroam password, which is also your edu email password.
+  * 
+  * @return
+  *    - true: Your board is connected to eduroam
+  */
+bool connect_to_eduroam(const char *eduroam_id, const char *eduroam_password);
+
+/**
+  * @brief  Connect to eduroam. (Blocking but w/ timeout)
+  * 
+  * @attention 1. The core you run this function on will be BLOCKED until eduroam connection established.
+  * @attention 2. This function does NOT append "@shanghaitech.edu.cn" to eduroam_id for you, plz include it on your own.
+  * @attention 3. Your eduroam password MAY BE DIFFERENT w/ your ShanghaiTech password, egate password or GAuth password, be careful.
+  *
+  * @param  eduroam_id: Your eduroam id, which is also your edu email address. 
+  * @param  eduroam_password: Your eduroam password, which is also your edu email password.
+  * @param  timeout: Stop blocking after assgined timeout, in microseconds.
+  *
+  * @return
+  *    - true: Your board is connected to eduroam
+  *    - false: Timeout
+  */
+bool connect_to_eduroam(const char *eduroam_id, const char *eduroam_password, int timeout);
 
 #endif
